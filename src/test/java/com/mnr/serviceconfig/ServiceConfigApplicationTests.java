4c89fa11add6d340d1a4b2d4c9f3a1a08090545c
Mounir Bkr <miirbri1@gmail.com>
2022-11-09 00:36:06 +0100

    commit 0 "fin configuration service for microservice "

---
+++ src/test/java/com/mnr/serviceconfig/ServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnr.serviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
